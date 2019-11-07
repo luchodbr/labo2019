4ee73c35410e6407f4ef5046409d5fb347065087
luchodbr@gmail.com <luchodbr@gmail.com>
2019-11-07 20:08:17 -0300

    event

---
+++ Ej68/ClassLibrary/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public delegate void DelegadoString(string msg);
+
+    public class Persona
+    {
+        public event DelegadoString EventoString;
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if (value != Nombre)
+                {
+                    this.EventoString.Invoke("Se cambio el nombre");
+                    nombre = value;
+                }
+            }
+
+        }
+        private string apellido;
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set {
+                if (value != Apellido)
+                {
+                    this.EventoString.Invoke("Se cambio el nombre");
+                    apellido = value;
+                }
+            }
+        }
+        public Persona()
+        {
+
+        }
+        public string Mostrar()
+        {
+            return ("nombre es: " + this.Nombre + "apellido es: " + this.Apellido);
+        }
+    }
+}
---
+++ Ej68/Ej68/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+namespace Ej68
+{
+    public partial class Form1 : Form
+    {
+        public Persona p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void NotificarCambio(string msj)
+        {
+            MessageBox.Show(msj);
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            if (this.p == null)
+            {
+                this.p = new Persona();
+                this.p.EventoString += NotificarCambio;
+                this.btnCrear.Text = "Actualizar";
+            }
+                this.p.Nombre = this.txtNombre.Text;
+                this.p.Apellido = this.txtApellido.Text;
+        }
+    }
+}
